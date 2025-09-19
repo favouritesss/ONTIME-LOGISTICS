5d710ec330064f3431362d6dfb1e258b1d3b75b8
favouritesss <your_github_email@example.com>
2025-09-19 12:19:39 -0400

    Fix prisma client generation and config

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
